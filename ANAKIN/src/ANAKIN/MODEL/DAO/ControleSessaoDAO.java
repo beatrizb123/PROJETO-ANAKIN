88a6527e51f0729108af217676742563d84e85ed
beatrizb123 <2022327305@ifam.edu.br>
2023-08-20 13:09:58 -0400

    Atualização - classes

---
+++ ANAKIN/src/ANAKIN/MODEL/DAO/ControleSessaoDAO.java
@@ -0,0 +1,30 @@
+package ANAKIN.MODEL.DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
+import ANAKIN.MODEL.VO.ControleSessaoVO;
+
+//precisa de modificação com fk do user
+public class ControleSessaoDAO {
+	Connection conn;
+	PreparedStatement PSTM;
+	
+	public void salvarInformacoes(ControleSessaoVO controle) {
+		conn = new ConexaoDAO().conectabd();
+		String sql = "insert into controle_sessao (nome_sessao, inventario_sessao, anotacoes_sessao) values (?, ?, ?)";
+		try {
+			PSTM = conn.prepareStatement(sql);
+			PSTM.setString(1, controle.getNome_sessao());
+			PSTM.setString(2, controle.getInventario_sessao());
+			PSTM.setString(3, controle.getAnotacoes_sessao());
+			PSTM.execute();
+			PSTM.close();
+		} catch (SQLException erro) {
+			JOptionPane.showMessageDialog(null, "ControleSessaoDAO: " + erro);
+		}	
+	}
+}
